60c8ca4ad92d2ee5caa52f5f83b6a180bf54fb8e
alisha0905 <130990762+alisha0905@users.noreply.github.com>
2024-07-07 22:39:11 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+texto=input("Ingrese el texto a manipular: ")
+print(texto.upper())
+print(texto.lower())
+print(texto[0:2])
+print(texto[-2:])
+print(texto.count(texto[-1]))
+print(texto[::-1])
